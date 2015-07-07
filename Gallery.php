eb2b8897ede0f7721137486ce1fa7b7646af5b76
Bhagwat Chouhan <bhagwat.chouhan@vulpinecode.com>
2015-07-07 19:27:33 +0530

    Update widget to extend base class.

--- Gallery.php
+++ Gallery.php
@@ -10,26 +10,11 @@ use cmsgears\core\common\services\GalleryService;
 
 // TODO: Add a bootstrap view apart from cmgtools
 
-class Gallery extends Widget {
+class Gallery extends \cmsgears\core\widgets\BaseWidget {
 
 	// Variables ---------------------------------------------------
 
 	// Public Variables --------------------
-
-	/**
-	 * The html options for the parent container.
-	 */
-	public $options;
-
-	/**
-	 * The path at which view file is located. It can have alias. By default it's the views folder within widget directory.
-	 */
-	public $viewPath	= null;
-
-	/**
-	 * The view file used to render widget.
-	 */
-	public $view		= 'simple';
 
 	/**
 	 * The gallery name.
@@ -52,54 +37,39 @@ class Gallery extends Widget {
 	// yii\base\Widget
 
 	/**
-	 * The method returns the view path for this widget if set while calling widget. 
-	 */
-	public function getViewPath() {
-
-		if( isset( $this->viewPath ) ) {
-
-			return $this->viewPath;
-		}
-
-		return parent::getViewPath();
-	}
-
-	/**
 	 * @inheritdoc
 	 */
     public function run() {
 
         if( !isset( $this->galleryName ) ) {
 
-            throw new InvalidConfigException( "Gallery name is required." );
+            throw new InvalidConfigException( 'Gallery name is required.' );
         }
 
 		$gallery		= GalleryService::findByName( $this->galleryName );
 		$items 			= [];
 		$galleryHtml	= '';
 
+        if( !isset( $gallery ) ) {
+
+            throw new InvalidConfigException( 'Gallery does not exist. Please create it via admin having name set to $this->galleryName.' );
+        }
+
 		// Paths
-		$wrapperPath	= $this->view . "/wrapper";
-		$itemPath		= $this->view . "/item";
+		$wrapperPath	= $this->viewFile . "/wrapper";
+		$itemPath		= $this->viewFile . "/item";
 
-		if( isset( $gallery ) ) {
+		// Generate Items Html
 
-			// Generate Items Html
+		$gitems = $gallery->files;
 
-			$gitems = $gallery->files;
-	
-	        foreach( $gitems as $item ) {
-	
-	            $items[] = $this->render( $itemPath, [ 'item' => $item ] );
-	        }
+        foreach( $gitems as $item ) {
 
-			$itemsHtml		= implode( "\n", $items );
-			$galleryHtml	= $this->render( $wrapperPath, [ 'gallery' => $gallery, 'itemsHtml' => $itemsHtml ] );
-		}
-		else {
+            $items[] = $this->render( $itemPath, [ 'item' => $item ] );
+        }
 
-			$galleryHtml	= "<p>Gallery does not exist. Please create it via admin having name set to $this->galleryName.</p>";
-		}
+		$itemsHtml		= implode( "\n", $items );
+		$galleryHtml	= $this->render( $wrapperPath, [ 'gallery' => $gallery, 'itemsHtml' => $itemsHtml ] );
 
         return Html::tag( 'div', $galleryHtml, $this->options );
     }
